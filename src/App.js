8f326dc4c4e03bf6c6bffe9b1169f2ee8027e103
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-27 22:04:38 +0000

    install router and add 3 routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import User from './pages/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/user">User</Link>
+            </li>
+          </ul>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/user" component={User}/>
+            </Switch>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
