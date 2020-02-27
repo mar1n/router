e8b2eff7fca20650728946c3546b68251c63cdad
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-27 22:49:18 +0000

    go back to route

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { useLocation } from "react-router";
+import { useLocation, useHistory } from "react-router";
 
 export default function About() {
     const location = useLocation();
+    const history = useHistory();
     console.log(location);
+    
+    function goBackHandle() {
+        history.goBack();
+    }
     return (
         <>
             <div>About</div>
             <div>Location = {location.pathname}</div>
             <div>From = {location.state.from}</div>
+            <button onClick={goBackHandle}>Go Back</button>
         </>
     );
 }
